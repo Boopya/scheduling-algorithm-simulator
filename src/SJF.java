4d69ac37bdd231ca8e5ff694eca9cef80ee28d8b
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-03-01 14:19:12 +0800

    :tada:

--- src/SJF.java
+++ src/SJF.java
@@ -1,49 +1,103 @@
 
-import java.awt.Color;
-import java.awt.Dimension;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import javax.swing.*;
 
 public class SJF {
     JFrame frame;
-    JButton p1, p2, p3, p4;
-	int bt1, bt2, bt3, bt4;
+    JLabel p1, p2, p3, p4, avg;
+	JPanel pane;
+	double bt1, bt2, bt3, bt4, total;
+	int x1, x2, x3, x4;
+	SJFAttributes[] sjf;
+	SJFAttributes sjfTemp;
     
     public SJF(String p1, String p2, String p3, String p4) {
-		frame = new JFrame("SJF");
+		sjf = new SJFAttributes[4];
 		
-		this.bt1 = Integer.parseInt(p1);
-		this.bt2 = Integer.parseInt(p2);
-		this.bt3 = Integer.parseInt(p3);
-		this.bt4 = Integer.parseInt(p4);
+		for(int i = 0; i < sjf.length; i++) {
+			sjf[i] = new SJFAttributes();
+		}
 		
-		this.p1 = new JButton("P1");
-		this.p1.setBounds(5,5,bt1,20);
-		this.p1.setBackground(Color.BLACK);
+		sjf[0].setProcess("P1");
+		sjf[0].setBurstTime(Integer.parseInt(p1));
+		sjf[1].setProcess("P2");
+		sjf[1].setBurstTime(Integer.parseInt(p2));
+		sjf[2].setProcess("P3");
+		sjf[2].setBurstTime(Integer.parseInt(p3));
+		sjf[3].setProcess("P4");
+		sjf[3].setBurstTime(Integer.parseInt(p4));
+		sjfTemp = new SJFAttributes();
 		
-		this.p2 = new JButton("P2");
-		this.p2.setBounds(5+bt1,5,bt2,20);
-		this.p2.setBackground(Color.BLUE);
+		for(int i = 0; i < sjf.length; i++) {
+			for(int j = 0; j < sjf.length-i-1; j++) {
+				if(sjf[j].getBurstTime() > sjf[j+1].getBurstTime()) {
+					 sjfTemp = sjf[j];
+					 sjf[j] = sjf[j+1];
+					 sjf[j+1] = sjfTemp;
+				}
+			}
+		}
 		
-		this.p3 = new JButton("P3");
-		this.p3.setBounds(5+bt1+bt2,5,bt3,20);
-		this.p3.setBackground(Color.CYAN);
+		bt1 = sjf[0].getBurstTime();
+		bt2 = sjf[1].getBurstTime();
+		bt3 = sjf[2].getBurstTime();
+		bt4 = sjf[3].getBurstTime();
+		total = bt1 + bt2 + bt3 + bt4;
+		bt1 = bt1 / total * 500;
+		bt2 = bt2 / total * 500;
+		bt3 = bt3 / total * 500;
+		bt4 = bt4 / total * 500;
+		x1 = (int) bt1;
+		x2 = (int) bt2;
+		x3 = (int) bt3;
+		x4 = (int) bt4;
 		
-		this.p4 = new JButton("P4");
-		this.p4.setBounds(5+bt1+bt2+bt3,5,bt4,20);
-		this.p4.setBackground(Color.DARK_GRAY);
-		
-		frame.getContentPane().add(this.p1);
-		frame.getContentPane().add(this.p2);
-		frame.getContentPane().add(this.p3);
-		frame.getContentPane().add(this.p4);
-		frame.setSize(new Dimension(500,500));
-		frame.setLayout(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		frame.setLocationRelativeTo(null);
-		frame.pack();
+		initComponents();
     }
+	
+	public void initComponents() {
+		frame = new JFrame("FCFS");
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pane = new JPanel();
+		pane.setLayout(null);
+		frame.add(pane);
+		
+		p1 = new JLabel(sjf[0].getProcess(), SwingConstants.CENTER);
+		p1.setBounds(0, 200, (int)bt1, 100);
+		p1.setBackground(Color.BLACK);
+		p1.setOpaque(true);
+		pane.add(p1);
+		
+		p2 = new JLabel(sjf[1].getProcess(), SwingConstants.CENTER);
+		p2.setBounds(x1, 200, x2, 100);
+		p2.setBackground(Color.BLUE);
+		p2.setOpaque(true);
+		pane.add(p2);
+		
+		p3 = new JLabel(sjf[2].getProcess(), SwingConstants.CENTER);
+		p3.setBounds(x1+x2, 200, x3, 100);
+		p3.setBackground(Color.CYAN);
+		p3.setOpaque(true);
+		pane.add(p3);
+		
+		p4 = new JLabel(sjf[3].getProcess(), SwingConstants.CENTER);
+		p4.setBounds(x1+x2+x3, 200, x4, 100);
+		p4.setBackground(Color.DARK_GRAY);
+		p4.setOpaque(true);
+		pane.add(p4);
+		
+		avg = new JLabel("Average Waiting Time: " +
+				(4 * (bt1 * total / 500) + 
+				 3 * (bt2 * total / 500) + 
+				 2 * (bt3 * total / 500) + 
+				 1 * (bt4 * total / 500)) / 4,
+				SwingConstants.CENTER);
+		avg.setBounds(100, 300, 300, 150);
+		pane.add(avg);
+	}
 }
---
+++ src/SJFAttributes.java
@@ -0,0 +1,20 @@
+public class SJFAttributes {
+	String p;
+	int bt;
+	
+	public void setProcess(String p) {
+		this.p = p;
+	}
+	
+	public String getProcess() {
+		return p;
+	}
+	
+	public void setBurstTime(int bt) {
+		this.bt = bt;
+	}
+	
+	public int getBurstTime() {
+		return bt;
+	}
+}
